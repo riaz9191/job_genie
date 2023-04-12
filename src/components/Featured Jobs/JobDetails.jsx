5918056c06cd7b5892c66075aa37f29c802244d3
riaz9191 <riazahammed9191@gmail.com>
2023-04-12 09:47:50 +0600

    show more btn done

--- src/components/Featured Jobs/JobDetails.jsx
+++ src/components/Featured Jobs/JobDetails.jsx
@@ -3,7 +3,7 @@ import SingleJob from "./SingleJob";
 import { Link } from "react-router-dom";
 
 const JobDetails = ({ jobs }) => {
-  const [showMore,setShowMore] = useState(false);
+  const [showMore, setShowMore] = useState(false);
   return (
     <div>
       <div className="my-container">
@@ -14,19 +14,25 @@ const JobDetails = ({ jobs }) => {
           Explore thousands of job opportunities with all the information you
           need. Its your future
         </p>
-        <ul>
+        {showMore ? (
           <div className="grid lg:grid-cols-2 gap-8 justify-between mt-12">
+            {jobs.data.map((job) => (
+              <SingleJob key={job.id} job={job}></SingleJob>
+            ))}
+          </div>
+        ) : (
+          <div>
+            <div className="grid lg:grid-cols-2 gap-8 justify-between mt-12">
             {jobs.data.slice(0, 4).map((job) => (
               <SingleJob key={job.id} job={job}></SingleJob>
             ))}
-            
           </div>
-          <div className="st mx-auto mt-10 w-24 st text-white pr-1 pl-1 pt-2 pb-2 rounded-md ">
-            <button className="">Show more</button>
-            
+            <div className="st mx-auto mt-10 w-24 st text-white pr-1 pl-1 pt-2 pb-2 rounded-md ">
+              <button onClick={()=> setShowMore(!showMore)} className="">Show more</button>
+            </div>
           </div>
-        </ul>
-          
+        )}
+        <ul></ul>
       </div>
     </div>
   );
